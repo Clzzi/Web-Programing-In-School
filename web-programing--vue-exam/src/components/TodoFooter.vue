a170708b3c77d65b7ba18c26c621e875bd6d9d8b
Clzzi <sonmin1407@dgsw.hs.kr>
2021-06-03 21:03:33 +0900

    Add: TodoFooter / TodoController / TodoInput

---
+++ web-programing--vue-exam/src/components/TodoFooter.vue
@@ -0,0 +1,3 @@
+<template>
+  <footer class="TodoFooter">Made By Clzzi 20210603</footer>
+</template>
---
+++ web-programing--vue-exam/src/components/TodoInput.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="TodoInput">
+    <input type="text" class="TodoInput-Input" placeholder="Write Your TODO" />
+    <button class="TodoInput-AddBtn">ADD</button>
+  </div>
+</template>
