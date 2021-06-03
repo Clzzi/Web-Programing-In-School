9e2b02c6e45ae3ea13ac9763c3099895329f0386
Clzzi <sonmin1407@dgsw.hs.kr>
2021-06-03 20:50:47 +0900

    Add: TodoHeader

---
+++ web-programing--vue-exam/src/components/TodoHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <span class="header-Title">My ToDo</span>
+    <div class="header-Date">{{ date }}</div>
+  </header>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        date: ""
+      }
+    },
+    created() {
+      const now = new Date();
+      const month = now.getMonth() + 1;
+      const date = now.getDate();
+      const weekList = new Array("Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+      const week = weekList[now.getDay()];
+      this.date = `${month}-${date} ${week}`;
+    }
+  }
+</script>
