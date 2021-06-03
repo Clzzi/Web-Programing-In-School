6ea7a14f660f36c0234adc67b9067a3303d8761f
Clzzi <sonmin1407@dgsw.hs.kr>
2021-06-03 20:58:31 +0900

    Add: TodoList & TodoTitle

---
+++ web-programing--vue-exam/src/components/TodoList.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul class="TodoList">
+    <li class="TodoList-Item">
+      <input type="checkbox" id="list-item-1" />
+      <label for="list-item-1">
+        <p class="list-text">hi</p>
+      </label>
+      <p class="list-data">5/26</p>
+      <button class="list-delete">Delete</button>
+    </li>
+  </ul>
+</template>
