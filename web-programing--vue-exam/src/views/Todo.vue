0fec267a65e9ab4f9eac51ea377f39bf52968ea6
Clzzi <sonmin1407@dgsw.hs.kr>
2021-06-03 20:46:14 +0900

    Add: Todo Components & Todo Views

---
+++ web-programing--vue-exam/src/views/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="todo">
+    <TodoHeader />
+    <TodoTitle />
+    <TodoInput />
+    <TodoController />
+    <TodoList />
+    <TodoFooter />
+  </div>
+</template>
+
+<script>
+import TodoHeader from "../components/TodoHeader";
+import TodoTitle from "../components/TodoTitle";
+import TodoInput from "../components/TodoInput";
+import TodoList from "../components/TodoList";
+import TodoController from "../components/TodoController";
+import TodoFooter from "../components/TodoFooter";
+
+export default {
+  name: "ToDoList",
+  components: {
+    TodoHeader,
+    TodoTitle,
+    TodoInput,
+    TodoController,
+    TodoList,
+    TodoFooter,
+  },
+};
+</script>
