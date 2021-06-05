443a72064d66a2d7611422951dca1d0bcf06849d
Clzzi <sonmin1407@dgsw.hs.kr>
2021-06-05 16:41:16 +0900

    Fix: Nav Bar Contents

--- web-programing--vue-exam/src/App.vue
+++ web-programing--vue-exam/src/App.vue
@@ -1,31 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Todo</router-link> |
-      <router-link to="/intro">Intro</router-link>
+  <div class="nav">
+    <div class="nav-Logo">Vue Exam</div>
+    <div class="nav-Content">
+      <router-link to="/">ToDoList</router-link>
+      <router-link to="/intro">IntroDuce</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.nav {
+  width: 100vw;
+  height: 60px;
+  display: flex;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
+  align-items: center;
+  vertical-align: middle;
+  border-bottom: 1px solid black;
+  &-Logo {
+    display: flex;
+    flex-wrap: 1;
+    font-size: 2rem;
     font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    color: #0080ff;
+    margin-right: 30px;
+    height: 100%;
+    line-height: 60px;
+    margin-left: 30px;
+  }
+  &-Content {
+    height: 100%;
+    line-height: 60px;
+    margin-left: 35%;
+    :nth-child(1) {
+      padding-right: 30px;
+    }
+    & > a {
+      font-size: 1.25rem;
+      text-decoration: none;
+      color: #1ec0ff;
+      &.router-link-exact-active {
+        color: #0080ff;
+      }
     }
   }
 }
